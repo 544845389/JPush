586ba08c92ee64fe62246e19e0ae59cda8f645f8
54484 <544845389@qq.com>
2018-11-05 20:31:12 +0800

    ...

---
+++ server-push/src/main/java/com/codingapi/ServerPushApplication.java
@@ -0,0 +1,12 @@
+package com.codingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerPushApplication.class, args);
+	}
+}
